d863a053fe36b30a6f6d447338c0703047df53fe
Shanul Haque <sh2553@srmist.edu.in>
2026-03-07 10:45:35 +0530

    UC2: Added UC2 banner feature

---
+++ uc2.java
@@ -0,0 +1,22 @@
+
+//uc2
+
+public class uc2 {
+    public static void main(String[] args) {
+        
+    System.out.println("     ***      "  + " " + "     ***      " + "**** **    " + "   ******");
+    
+    System.out.println("  **     **   "  + " " + "  **     **   " + "**     ** " + " **      ");
+
+    System.out.println("**         ** "  + " " + "**         ** " + "**     ** " + "**       ");
+
+    System.out.println("**         ** "  + " " + "**         ** " + "*** **     " + "  ****   ");
+
+    System.out.println("**         ** "  + " " + "**         ** " + "***        " + "       **");
+
+    System.out.println(" **       **  "  + " " + " **       **  " + "***        " + "      ** ");
+
+    System.out.println("     ***      "  + " " + "     ***      " + "***        " + "******   ");
+    
+}
+}
